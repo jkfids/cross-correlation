afe3cbdc721c66a58703c720653465a629bb1c4d
Fidel <jfidelkam@gmail.com>
2021-04-01 15:11:37 +1100

    Update test.py

--- crosscorrelation/test.py
+++ crosscorrelation/test.py
@@ -15,15 +15,19 @@ from crosscorrelation import norm_crosscorr
 f = np.array(list(range(1000)))
 g = np.roll(f, 100)
 
-R = norm_crosscorr(f, f)
-X = np.linspace(-1, 1, len(R))
+R = norm_crosscorr(f, g)
+X = np.linspace(-.5, .5, len(R))
 
 fig, ax = plt.subplots()
 ax.plot(X, R)
 ax.set_title('Cross Correlation')
 ax.set_xlabel('Lag')
 ax.set_ylabel('Correlation (Normalized)')
+ax.xaxis.set_ticks(np.arange(-0.5, 0.6, 0.1))
+ax.grid(True)
 fig.set_size_inches(12, 4)
 fig.set_dpi(144)
 
-print(np.argmax(R))
+arg_max = np.argmax(R)/(len(R)-1)-0.5
+print(arg_max)
+print(len(f)-1-np.argmax(R))
